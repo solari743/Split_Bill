c7b039a0580fab65c2cdea25fe6b01370d866376
Kevin Bermejo <myhomiebruh420@gmail.com>
2026-01-07 16:24:02 -0500

    menu start

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const MyTabs = createBottomTabNavigator({
-  screens: {
-    Home: HomeScreen,
-    Profile: ProfileScreen,
-  },
-});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Menu Page</Text>
       <StatusBar style="auto" />
     </View>
   );
